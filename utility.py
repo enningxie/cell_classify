7e189f316d925e5c81cf153ea7c6e4db5b4c6723
enningxie <enningxie@163.com>
2018-03-26 15:29:12 +0800

    process raw datra

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 BASE_PATH = '/home/enningxie/Documents/DataSets/CELL_images'
 HSIL_PATH = os.path.join(BASE_PATH, 'HSIL')
@@ -6,8 +7,39 @@ LSIL_PATH = os.path.join(BASE_PATH, 'LSIL')
 NILM_PATH = os.path.join(BASE_PATH, 'NILM')
 
 
-def process_rawdata():
-    # rename_datafiles(NILM_PATH, 'NILM')
+def mkdir_pwd(base_dir, join_dir):
+    ex_dir = os.path.join(base_dir, join_dir)
+    if not os.path.exists(ex_dir):
+        os.mkdir(ex_dir)
+    return ex_dir
+
+
+train_path = mkdir_pwd(BASE_PATH, 'train_data')
+test_path = mkdir_pwd(BASE_PATH, 'test_data')
+
+
+def move_to(src_path, rate):
+    src_len = len(os.listdir(src_path))
+    train_len = int(src_len * rate)
+    for i, f in enumerate(os.listdir(src_path)):
+        if not i < train_len:
+            break
+        src_name = os.path.join(src_path, f)
+        to_name = os.path.join(train_path, f)
+        shutil.move(src_name, to_name)
+    for f in os.listdir(src_path):
+        src_name = os.path.join(src_path, f)
+        to_name = os.path.join(test_path, f)
+        shutil.move(src_name, to_name)
+
+
+def split_train_test_set(rate):
+    # move_to(HSIL_PATH, rate)
+    move_to(LSIL_PATH, rate)
+    move_to(NILM_PATH, rate)
+    os.removedirs(HSIL_PATH)
+    os.removedirs(LSIL_PATH)
+    os.removedirs(NILM_PATH)
 
 
 def rename_datafiles(path, subname):
@@ -16,10 +48,19 @@ def rename_datafiles(path, subname):
         tmp_path = os.path.join(path, f)
         tmp = os.path.splitext(f)
         count += 1
-        print(count)
+        # print(count)
         os.rename(tmp_path, os.path.join(path, subname+'_'+str(count)+tmp[-1]))
 
 
+def main():
+    # rename_op
+    rename_datafiles(HSIL_PATH, 'HSIL')
+    rename_datafiles(LSIL_PATH, 'HSIL')
+    rename_datafiles(NILM_PATH, 'NILM')
+
+    # split train/test set
+    split_train_test_set(0.7)
+
 
 if __name__ == '__main__':
-    process_rawdata()
+    main()
