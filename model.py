b8fe2b698137da894d303512bb3311ecd11c4b92
enningxie <enningxie@163.com>
2018-04-02 20:42:18 +0800

    model

--- model.py
+++ model.py
@@ -10,18 +10,22 @@ validation_dir = '/home/enningxie/Documents/DataSets/CELL_IMAGES/CELL_images/tes
 
 model = models.Sequential()
 
-model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPool2D((2, 2)))
-model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
-model.add(layers.MaxPool2D((2, 2)))
-model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
-model.add(layers.MaxPool2D((2, 2)))
-model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
-model.add(layers.MaxPool2D((2, 2)))
-model.add(layers.Flatten())
-# add dropout layer
-model.add(layers.Dropout(0.2))
-model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Conv2D(32, (3, 3)))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Conv2D(64, (3, 3)))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+model.add(layers.Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(layers.Dense(64))
+model.add(layers.Activation('relu'))
+model.add(layers.Dropout(0.7))
 model.add(layers.Dense(3, activation='softmax'))
 
 print(model.summary())
@@ -36,9 +40,19 @@ model.compile(loss=losses.categorical_crossentropy,
 train_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
+train_datagen_aug = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
 train_generator = train_datagen.flow_from_directory(
     train_dir,  # target directory
-    target_size=(32, 32),  # resize all images to 150x150
+    target_size=(50, 50),  # resize all images to 150x150
     batch_size=32,
     class_mode='categorical'  # binary
 )
@@ -52,11 +66,11 @@ validation_generator = test_datagen.flow_from_directory(
 
 # fitting the model using a batch generator
 history = model.fit_generator(
-    train_generator,
-    steps_per_epoch=1100,
+    train_datagen_aug,
+    steps_per_epoch=150,
     epochs=10,
     validation_data=validation_generator,
-    validation_steps=150
+    validation_steps=50
 )
 
 print(history.history)
